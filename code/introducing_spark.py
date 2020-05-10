c19bfea3d157bc60a558ecaed59d5403acc476ca
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-05-10 18:24:57 +0800

    Converting RDDs to DataFrame

--- code/introducing_spark.py
+++ code/introducing_spark.py
@@ -1,6 +1,6 @@
 from pyspark.shell import spark
 from pyspark.sql.types import Row
-from datetime import datetime
+
 sc = spark.sparkContext
 print(sc)
 simple_data = sc.parallelize([1, "Alice", 50])
@@ -10,6 +10,28 @@ print(simple_data.first())
 print(simple_data.take(2))
 print(simple_data.collect())
 
-df = spark.createDataFrame(simple_data)
-print(df)
+# df = (simple_data.toDF())
+# print(df)
 
+records = sc.parallelize([[1, "Alice", 50], [2, "Bob", 80]])
+print(records)
+print(records.collect())
+print(records.count())
+print(records.first())
+df = records.toDF()
+print(df)
+df.show()
+
+data = sc.parallelize([Row(id=1, name="Alice", score=50)])
+print(data)
+print(data.count)
+print(data.collect())
+df = data.toDF()
+df.show()
+
+data = sc.parallelize([Row(id=1, name="Alice", score=50),
+                       Row(id=2, name="Bob", score=80),
+                       Row(id=3, name="Charlee", score=75)])
+
+df = data.toDF()
+df.show()
