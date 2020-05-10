e93bf33e317909b8eed1b6574e3cdab41d3d3070
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-05-10 18:42:16 +0800

    Working With Complex Data Types

--- code/introducing_spark.py
+++ code/introducing_spark.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pyspark.shell import spark
 from pyspark.sql.types import Row
 
@@ -35,3 +37,24 @@ data = sc.parallelize([Row(id=1, name="Alice", score=50),
 
 df = data.toDF()
 df.show()
+
+complex_data = sc.parallelize([Row(col_float=1.44, col_integer=10, col_string="John")])
+complex_data_df = complex_data.toDF()
+complex_data_df.show()
+
+complex_data = sc.parallelize(
+    [Row(col_float=1.44, col_integer=10, col_string="John", col_boolean=True, col_list=[1, 2, 3])])
+complex_data_df = complex_data.toDF()
+complex_data_df.show()
+
+complex_data = sc.parallelize([Row(col_list=[1, 2, 3], col_dict={"k1": 0}, col_row=Row(a=10, b=20, c=30),
+                                   col_time=datetime(2014, 8, 1, 14, 1, 5)),
+                               Row(col_list=[1, 2, 3, 4, 5], col_dict={"k1": 0, "k2": 1}, col_row=Row(a=40, b=50, c=60),
+                                   col_time=datetime(2014, 8, 2, 14, 1, 6)),
+                               Row(col_list=[1, 2, 3, 4, 5, 6, 7], col_dict={"k1": 0, "k2": 1, "k3": 2},
+                                   col_row=Row(a=70, b=80, c=90),
+                                   col_time=datetime(2014, 8, 3, 14, 1, 7))
+                               ])
+
+complex_data_df = complex_data.toDF()
+complex_data_df.show()
